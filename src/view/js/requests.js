6c212ca1154d7d15ecb2209f282fb4b9c3cae345
Renato Oliveira <renato.loliveira89@gmail.com>
2023-10-31 13:43:48 -0300

    atualizando projeto
    Atualizado formulário de pesquisa, criado formulário para atulização.

--- src/view/js/requests.js
+++ src/view/js/requests.js
@@ -1,6 +1,6 @@
 // url do meu servidor
 const url = "http://localhost:3000";
-const itensSelecionados = [];
+const pessoaSelecionada = [];
 
 function cadastrar() {
   console.log('Enviando dados ao servidor...');
@@ -16,7 +16,7 @@ function cadastrar() {
     peso: document.querySelector('[name="peso"]').value
   }
 
-  fetch(url + '/user', {
+  fetch(`${url}/user`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -61,85 +61,210 @@ function cadastrar() {
 
 function listar() {
 
+  const pesquisaNome = document.querySelector('[class="pesquisar-pessoa"]').value;
+
+  console.log(pesquisaNome);
+
   destruirTabela();
 
   criarCabecalhoTabela();
 
-  fetch(url + '/pessoas')
-    .then(response => response.json())
-    .then(data => {
-      console.log('Lista recebida do servidor:', data);
-      // Faça algo com a lista recebida, como renderizá-la na interface do usuário
+  if (pesquisaNome == '' || pesquisaNome == null || pesquisaNome == 0) {
+    fetch(`${url}/pessoas`)
+      .then(response => response.json())
+      .then(data => {
+        console.log('Lista recebida do servidor:', data);
+        // Faça algo com a lista recebida, como renderizá-la na interface do usuário
 
-      const tabela = document.getElementById('tabela-pessoas'); // Suponha que você tenha um elemento HTML com o id 'lista' onde deseja exibir os dados
+        const tabela = document.getElementById('tabela-pessoas'); // Suponha que você tenha um elemento HTML com o id 'lista' onde deseja exibir os dados
 
-      data.forEach(item => {
-        const dataParse = new Date(item.data_nascimento);
+        data.rows.forEach(item => {
+          const dataParse = new Date(item.data_nascimento);
 
-        const trElement = document.createElement('tr');
-        const tdId = document.createElement('td');
-        const tdNome = document.createElement('td');
-        const tdCPF = document.createElement('td');
-        const tdDataNascimento = document.createElement('td');
-        const tdTelefone = document.createElement('td');
-        const tdEndereco = document.createElement('td');
-        const tdAltura = document.createElement('td');
-        const tdPeso = document.createElement('td');
+          const trElement = document.createElement('tr');
+          const tdId = document.createElement('td');
+          const tdNome = document.createElement('td');
+          const tdCPF = document.createElement('td');
+          const tdDataNascimento = document.createElement('td');
+          const tdTelefone = document.createElement('td');
+          const tdEndereco = document.createElement('td');
+          const tdAltura = document.createElement('td');
+          const tdPeso = document.createElement('td');
 
-        const tdAcao = document.createElement('td');
-        const imgApagar = document.createElement('img');
-        const imgEditar = document.createElement('img');
+          const tdAcao = document.createElement('td');
+          const imgApagar = document.createElement('img');
+          const imgEditar = document.createElement('img');
 
-        tdId.textContent = item.id;
-        tdId.hidden = true;
-        trElement.appendChild(tdId);
+          tdId.textContent = item.id;
+          tdId.hidden = true;
+          trElement.appendChild(tdId);
 
-        tdNome.textContent = item.nome;
-        trElement.appendChild(tdNome);
-        tdCPF.textContent = item.cpf;
-        trElement.appendChild(tdCPF);
-        tdDataNascimento.textContent = `${dataParse.getDate()}/${dataParse.getMonth()}/${dataParse.getFullYear()}`;
-        trElement.appendChild(tdDataNascimento);
-        tdTelefone.textContent = item.telefone;
-        trElement.appendChild(tdTelefone);
-        tdEndereco.textContent = item.endereco;
-        trElement.appendChild(tdEndereco);
-        tdAltura.textContent = item.altura;
-        trElement.appendChild(tdAltura);
-        tdPeso.textContent = item.peso;
-        trElement.appendChild(tdPeso);
+          tdNome.textContent = item.nome;
+          trElement.appendChild(tdNome);
+          tdCPF.textContent = item.cpf;
+          trElement.appendChild(tdCPF);
+          tdDataNascimento.textContent = `${dataParse.getDate()}/${dataParse.getMonth()+1}/${dataParse.getFullYear()}`;
+          trElement.appendChild(tdDataNascimento);
+          tdTelefone.textContent = item.telefone;
+          trElement.appendChild(tdTelefone);
+          tdEndereco.textContent = item.endereco;
+          trElement.appendChild(tdEndereco);
+          tdAltura.textContent = item.altura;
+          trElement.appendChild(tdAltura);
+          tdPeso.textContent = item.peso;
+          trElement.appendChild(tdPeso);
 
-        imgEditar.src = "../assets/images/editar-icon.png"
-        imgEditar.addEventListener('click', () => selecionarItem(item.id, item.nome));
-        tdAcao.appendChild(imgEditar);
+          imgEditar.src = "../assets/images/editar-icon.png"
+          imgEditar.addEventListener('click', () => editarPessoa(item));
+          tdAcao.appendChild(imgEditar);
 
-        imgApagar.src = "../assets/images/lixeira-icon.png";
-        imgApagar.addEventListener('click', () => apagarPessoa(item.id, item.nome));
-        tdAcao.appendChild(imgApagar);
+          imgApagar.src = "../assets/images/lixeira-icon.png";
+          imgApagar.addEventListener('click', () => apagarPessoa(item.id, item.nome));
+          tdAcao.appendChild(imgApagar);
 
-        trElement.appendChild(tdAcao);
+          trElement.appendChild(tdAcao);
 
-        tabela.appendChild(trElement);
+          tabela.appendChild(trElement);
+        });
+
+      })
+      .catch(error => {
+        console.error('Erro ao buscar a lista do servidor:', error);
+        // Trate os erros, se necessário
       });
+  } else {
+    fetch(`${url}/pessoa/${pesquisaNome}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log('Lista recebida do servidor:', data);
+        // Faça algo com a lista recebida, como renderizá-la na interface do usuário
 
-    })
-    .catch(error => {
-      console.error('Erro ao buscar a lista do servidor:', error);
-      // Trate os erros, se necessário
-    });
+        const tabela = document.getElementById('tabela-pessoas'); // Suponha que você tenha um elemento HTML com o id 'lista' onde deseja exibir os dados
+
+        data.rows.forEach(item => {
+          const dataParse = new Date(item.data_nascimento);
+
+          const trElement = document.createElement('tr');
+          const tdId = document.createElement('td');
+          const tdNome = document.createElement('td');
+          const tdCPF = document.createElement('td');
+          const tdDataNascimento = document.createElement('td');
+          const tdTelefone = document.createElement('td');
+          const tdEndereco = document.createElement('td');
+          const tdAltura = document.createElement('td');
+          const tdPeso = document.createElement('td');
+
+          const tdAcao = document.createElement('td');
+          const imgApagar = document.createElement('img');
+          const imgEditar = document.createElement('img');
+
+          tdId.textContent = item.id;
+          tdId.hidden = true;
+          trElement.appendChild(tdId);
+
+          tdNome.textContent = item.nome;
+          trElement.appendChild(tdNome);
+          tdCPF.textContent = item.cpf;
+          trElement.appendChild(tdCPF);
+          tdDataNascimento.textContent = `${dataParse.getDate()}/${dataParse.getMonth()}/${dataParse.getFullYear()}`;
+          trElement.appendChild(tdDataNascimento);
+          tdTelefone.textContent = item.telefone;
+          trElement.appendChild(tdTelefone);
+          tdEndereco.textContent = item.endereco;
+          trElement.appendChild(tdEndereco);
+          tdAltura.textContent = item.altura;
+          trElement.appendChild(tdAltura);
+          tdPeso.textContent = item.peso;
+          trElement.appendChild(tdPeso);
+
+          imgEditar.src = "../assets/images/editar-icon.png"
+          imgEditar.addEventListener('click', () => editarPessoa(item));
+          tdAcao.appendChild(imgEditar);
+
+          imgApagar.src = "../assets/images/lixeira-icon.png";
+          imgApagar.addEventListener('click', () => apagarPessoa(item.id, item.nome));
+          tdAcao.appendChild(imgApagar);
+
+          trElement.appendChild(tdAcao);
+
+          tabela.appendChild(trElement);
+        });
+
+      })
+      .catch(error => {
+        console.error('Erro ao buscar a lista do servidor:', error);
+        // Trate os erros, se necessário
+      });
+  }
 }
 
-function editarPessoa(id, nome) {
-  window.alert('Nada implementado ainda');
+function editarPessoa(pessoa) {
+  console.log(pessoa);
 
-  /* const index = itensSelecionados.indexOf(id);
+  const objetoPessoaSerializado = JSON.stringify(pessoa);
 
-  if (index !== -1) {
-    window.alert(`selecionado item ${id}`)
-    //itensSelecionados.splice(index, 1); // Remover item da lista de selecionados
+  const objetoPessoaCodificado = encodeURIComponent(objetoPessoaSerializado);
+
+  window.location.href = `alterar.html?pessoa=${objetoPessoaCodificado}`;
+}
+
+function carregarInfo() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const pessoaCodificada = urlParams.get('pessoa');
+
+  const pessoaSerializada = decodeURIComponent(pessoaCodificada);
+
+  const pessoa = JSON.parse(pessoaSerializada);
+
+  const dataParse = new Date(pessoa.data_nascimento);
+
+  document.querySelector('[name="id-pessoa"]').value = pessoa.id;
+  document.querySelector('[name="nome-pessoa-alt"]').value = pessoa.nome;
+  document.querySelector('[name="cpf-alt"]').value = pessoa.cpf;
+
+  if(dataParse.getMonth()+1 < 10) {
+    document.querySelector('[name="data-de-nascimento-alt"]').value = `${dataParse.getFullYear()}-0${dataParse.getMonth()+1}-${dataParse.getDate()}`;
   } else {
-    itensSelecionados.push(id); // Adicionar item à lista de selecionados
-  } */
+    document.querySelector('[name="data-de-nascimento-alt"]').value = `${dataParse.getFullYear()}-${dataParse.getMonth()+1}-${dataParse.getDate()}`;
+  }
+
+  document.querySelector('[name="telefone-alt"]').value = pessoa.telefone;
+  document.querySelector('[name="endereco-alt"]').value = pessoa.endereco;
+  document.querySelector('[name="altura-alt"]').value = pessoa.altura;
+  document.querySelector('[name="peso-alt"]').value = pessoa.peso;
+}
+
+function atualizarPessoa() {
+
+  const dadosPessoa = {
+    id: document.querySelector('[name="id-pessoa"]').value,
+    nome: document.querySelector('[name="nome-pessoa-alt"]').value,
+    cpf: document.querySelector('[name="cpf-alt"]').value,
+    data_nascimento: document.querySelector('[name="data-de-nascimento-alt"]').value,
+    telefone: document.querySelector('[name="telefone-alt"]').value,
+    endereco: document.querySelector('[name="endereco-alt"]').value,
+    altura: document.querySelector('[name="altura-alt"]').value,
+    peso: document.querySelector('[name="peso-alt"]').value
+  }
+
+  console.log(dadosPessoa);
+
+  fetch(`${url}/atualizar/${dadosPessoa.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify( {dadosPessoa} ),
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Exibir a resposta do back-end
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Erro ao alterar registro: ' + error);
+    });
+
 }
 
 function apagarPessoa(id, nome) {
@@ -166,19 +291,6 @@ function apagarPessoa(id, nome) {
           console.error('Ocorreu um erro:', error);
         });
     }
-  }
-}
-
-function selecionarItem(id, nome) {
-
-  const index = itensSelecionados.indexOf(id);
-
-  if (index !== -1) {
-    window.alert(`selecionado item ${id} + ${nome}`);
-    console.log(id);
-    //itensSelecionados.splice(index, 1); // Remover item da lista de selecionados
-  } else {
-    itensSelecionados.push(id); // Adicionar item à lista de selecionados
   }
 }
 
